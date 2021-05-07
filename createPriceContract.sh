3e500301b3f00bca8f88cf0eeb5e3e1c5e734a62
Jesulonimi21 <anjola4jeez@gmail.com>
2021-05-07 12:36:16 +0100

    initial Creation Of Contracts and .sh files

---
+++ createPriceContract.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+../../node/goal app create --creator BJLWZRWS7CYNNTBNYPGGHF7ZFJU2JOCFKXZSEUTVDC4INZEW63WZ2QLIHI --app-arg "int:200" --approval-prog ./Price.teal --global-byteslices 1 --global-ints 1 --local-byteslices 1 --local-ints 1 --clear-prog ./clearprice.teal 
