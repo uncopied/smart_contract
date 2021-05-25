f0487044214b85cdcf48f185baa7643fdb3b3a37
jesulonimi21 <anjola4jeez@gmail.com>
2021-05-25 23:51:30 +0100

    fixed calculation

---
+++ scripts/updateApp.sh
@@ -0,0 +1,3 @@
+ #!/bin/bash
+
+ goal app update --app-id 15953480 --from GJ2KFYI723EMIS76SNSG3TKHDSW7322AZZJXJNV3J35B4TIQVXFXJLB3PI --approval-prog ../contracts/Price.teal --clear-prog ../contracts/clearprice.teal
